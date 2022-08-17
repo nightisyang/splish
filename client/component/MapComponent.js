0d25a52c42a6de8cdd4fd0427e00afbbf1b30f92
nightisyang <shenyangc86@gmail.com>
2022-08-17 10:38:27 +0800

    clean up

--- client/component/MapComponent.js
+++ client/component/MapComponent.js
@@ -20,37 +20,23 @@ const MapComponent = ({
   const [isRegionChange, setRegionChange] = useState(false);
   const [newRegion, setNewRegion] = useState(regionInput);
 
+  // sets map ready state for useEffects
   const handleMapReady = () => {
     setMapReady(true);
-    console.log('Map ready, loading animation...');
   };
 
+  // animate to region, used in useEffect and is called when map is ready or region is changed
   const animateToRegionHolder = animateTime => {
     if (!mapRef.current) {
       console.log('Map ref is undefined');
     }
 
     if (mapRef.current) {
-      console.log('animating...');
-      console.log(animateToInput);
       mapRef.current.animateToRegion(animateToInput, animateTime);
     }
   };
 
-  useEffect(() => {
-    let animationDelay;
-    if (isMapReady) {
-      animationDelay = setTimeout(() => {
-        animateToRegionHolder(3500);
-      }, 500);
-    }
-
-    return () => {
-      clearTimeout(animationDelay);
-      console.log('Clearing timeout after region change...');
-    };
-  }, [isRegionChange, isMapReady]);
-
+  // renders marker and stored in state, only called when region change is complete
   const MarkerRender = () => {
     let markerRenderResult;
     if (type === 'main') {
@@ -62,10 +48,25 @@ const MapComponent = ({
     if (type === 'info') {
       markerRenderResult = <Marker coordinate={coordInput} />;
     }
+
+    // set marker element into state, component to be re-rendered after it's complete
     setMarkerRender(markerRenderResult);
-    console.log(`Map is ready: ${isMapReady}`);
     return;
   };
+
+  // executes animation only after map is ready, or region is changed, onRegionChange does not fire for android when browsing individual pages (same region?)
+  useEffect(() => {
+    let animationDelay;
+    if (isMapReady) {
+      animationDelay = setTimeout(() => {
+        animateToRegionHolder(3500);
+      }, 500);
+    }
+
+    return () => {
+      clearTimeout(animationDelay);
+    };
+  }, [isRegionChange]);
 
   return (
     <View style={{flex: 1, minHeight: 150, position: 'relative'}}>
@@ -78,19 +79,14 @@ const MapComponent = ({
         minZoomLevel={zoomLevelInput}
         liteMode={liteModeInput}
         onRegionChangeComplete={() => {
-          console.log(`%%%%%%%%% REGION CHANGE COMPLETE %%%%%%%%%%`);
+          //   console.log(`%%%%%%%%% REGION CHANGE COMPLETE %%%%%%%%%%`);
           MarkerRender();
-          if (type === 'info') {
-            console.log(`MapMarker is ready: ${isMarkerRendered}`);
-          } else {
-            console.log('Lots of markers rendered');
-          }
-
           setRegionChange(true);
         }}
         onMapReady={e => {
-          console.log('########### ONMAPREADY #############');
+          //   console.log('########### ONMAPREADY #############');
           handleMapReady();
+          setMarkerRender(null);
         }}
         zoomControlEnabled={true}>
         {isMarkerRendered}
