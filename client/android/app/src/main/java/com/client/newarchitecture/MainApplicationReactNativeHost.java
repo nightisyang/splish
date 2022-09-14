5aa75446b60d5f028d6c01cb34742933cdcd61e4
nightisyang <shenyangc86@gmail.com>
2022-09-14 15:09:20 +0800

    Modified name for production

--- client/android/app/src/main/java/com/client/newarchitecture/MainApplicationReactNativeHost.java
+++ client/android/app/src/main/java/com/client/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.client.newarchitecture;
+package com.splish14092022.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.client.BuildConfig;
-import com.client.newarchitecture.components.MainComponentsRegistry;
-import com.client.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.splish14092022.BuildConfig;
+import com.splish14092022.newarchitecture.components.MainComponentsRegistry;
+import com.splish14092022.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
